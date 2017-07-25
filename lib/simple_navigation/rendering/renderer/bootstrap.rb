ae6674ba58a13cffd48ff2907a8203e1e36a7c76
Dan Petker <dan.petker@vidyard.com>
2017-07-25 11:35:01 -0400

    Output buttons for dropdown toggles

--- lib/simple_navigation/rendering/renderer/bootstrap.rb
+++ lib/simple_navigation/rendering/renderer/bootstrap.rb
@@ -3,9 +3,12 @@ module SimpleNavigation
     class Bootstrap < SimpleNavigation::Renderer::Base
       def render(item_container)
         return '' if respond_to?(:skip_if_empty?) && skip_if_empty? && item_container.empty?
+
         config_selected_class = SimpleNavigation.config.selected_class
         SimpleNavigation.config.selected_class = 'active'
+
         list_content = item_container.items.inject([]) do |list, item|
+
           li_options = item.html_options.reject {|k, v| k == :link}
           icon = li_options.delete(:icon)
           dropdown = item_container.dropdown.nil? ? true : item_container.dropdown
@@ -13,6 +16,7 @@ module SimpleNavigation
           split = (include_sub_navigation?(item) and li_options.delete(:split)) if li_options.include?(:split)
           dropdown = (include_sub_navigation?(item) and li_options.delete(:dropdown)) if li_options.include?(:dropdown)
           li_content = tag_for(item, item.name, icon, split, dropdown)
+
           if include_sub_navigation?(item)
             if split
               lio = li_options.dup
@@ -22,47 +26,65 @@ module SimpleNavigation
               li_options[:id] = nil
               li_content = tag_for(item)
             end
+
             item.sub_navigation.dom_class = [item.sub_navigation.dom_class, dropdown ? 'dropdown-menu' : nil, split ? 'pull-right' : nil].flatten.compact.join(' ')
             li_content << render_sub_navigation_for(item)
             li_options[:class] = [li_options[:class], dropdown ? 'dropdown' : nil, split ? 'dropdown-split-right' : nil].flatten.compact.join(' ')
           end
+
           list << content_tag(:li, li_content, li_options)
         end.join
+
         SimpleNavigation.config.selected_class = config_selected_class
+
         if item_container.respond_to?(:dom_attributes)
           dom_attributes = item_container.dom_attributes
         else
           # supports simple-navigation before the ItemContainer#dom_attributes
           dom_attributes = {:id => item_container.dom_id, :class => item_container.dom_class}
         end
+
         content_tag(:ul, list_content, dom_attributes)
       end
 
       protected
 
       def tag_for(item, name = '', icon = nil, split = false, dropdown = false)
-        unless item.url or include_sub_navigation?(item)
+        include_sub_nav = include_sub_navigation?(item)
+
+        unless item.url or include_sub_nav
           return item.name
         end
+
         url = item.url
         link = Array.new
         link << content_tag(:i, '', :class => [icon].flatten.compact.join(' ')) unless icon.nil?
         link << name
-        if include_sub_navigation?(item)
+
+        if include_sub_nav
           item_options = item.html_options
           item_options[:link] = Hash.new if item_options[:link].nil?
           item_options[:link][:class] = Array.new if item_options[:link][:class].nil?
+
           unless split
             if dropdown
               item_options[:link][:class] << 'dropdown-toggle'
               item_options[:link][:'data-toggle'] = 'dropdown'
-              item_options[:link][:'data-target'] = '#'
+              item_options[:link][:type] = 'button'
+              item_options[:link][:'aria-haspopup'] = true
+              item_options[:link][:'aria-expanded'] = false
             end
             link << content_tag(:b, '', :class => 'caret')
           end
+
           item.html_options = item_options
         end
-        link_to(link.join(" ").html_safe, url, options_for(item))
+
+        if include_sub_nav && dropdown
+          content_tag(:button, link.join(" ").html_safe, options_for(item))
+        else
+          link_to(link.join(" ").html_safe, url, options_for(item))
+        end
       end
 
     end
